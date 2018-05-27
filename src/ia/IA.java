bebd9d40d5de66ba5776c2c65863942e0a382bcb
Octaviop15 <octaviopic15@gmail.com>
2018-05-27 00:26:32 -0300

    a

---
+++ src/ia/IA.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia;
+
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Rule;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+
+
+
+
+
+public class IA {
+
+   
+    public static void main(String[] args) {
+        
+        //carga de archivo
+        String fileName = "C:\\Users\\Octavio\\Desktop\\Nueva carpeta\\avip.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        // Error while loading?
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+        
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+       
+       // para que muestre las graficas
+        Variable edad = fis.getVariable("edad");
+        Variable habito = fis.getVariable("habito");
+        Variable tamaño = fis.getVariable("tamano");
+        Variable alojamiento = fis.getVariable("alojamiento");
+        Variable transporte = fis.getVariable("transporte");
+        Variable sexo = fis.getVariable("sexo");
+        JFuzzyChart.get().chart(fis.getFunctionBlock("avip"));
+         
+
+      
+ 
+
+       // Set inputs
+        fis.setVariable("transporte", interfaz.getTrasnporte());
+        fis.setVariable("edad", interfaz.getEdad());
+        fis.setVariable("sexo",interfaz.getSexo());
+        fis.setVariable("tamano", interfaz.getCantPersonas());
+        fis.setVariable("alojamiento", interfaz.getAlojamiento());
+
+        // Evaluate
+        fis.evaluate();
+
+       
+     
+
+        // Show output variable
+       
+        Variable coincidencia =  fis.getVariable("coincidencia");
+        JFuzzyChart.get().chart(coincidencia, coincidencia.getDefuzzifier(), true);
+        System.out.println("Output value:" + fis.getVariable("coincidencia").getValue()); 
+    }
+        
+    
+        
+        
+        
+        
+        
+        //DE PRUEBA 
+        
+       /* 
+        String fileName = "C:\\Users\\Octavio\\Desktop\\Nueva carpeta\\tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        // Error while loading?
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+        
+        Variable service = fis.getVariable("service");
+        JFuzzyChart.get().chart(fis.getFunctionBlock("tipper"));
+
+      
+ 
+
+       // Set inputs
+        fis.setVariable("service", 3);
+        fis.setVariable("food", 7);
+
+        // Evaluate
+        fis.evaluate();
+
+
+        // Show output variable
+       
+        Variable tip =  fis.getVariable("tip");
+        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+        System.out.println("Output value:" + fis.getVariable("tip").getValue()); 
+        
+        
+
+        // Print ruleSet
+        //System.out.println(fis);   
+       
+        
+        
+        
+   fis.setVariable("service", 3); // Set inputs
+    fis.setVariable("food", 7);
+    fis.evaluate(); // Evaluate
+    System.out.println("Output value:" + fis.getVariable("tip").getValue()); // Show output variable
+
+    // Show each rule (and degree of support)
+    for( Rule r : fis.getFunctionBlock("tipper").getFuzzyRuleBlock("No1").getRules() )
+      System.out.println(r); 
+        
+        
+      */
+
+
+
+       
+    
+    
+}
